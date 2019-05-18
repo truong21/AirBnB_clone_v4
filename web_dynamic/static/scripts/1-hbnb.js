b93edcb8cb894a4be96a48a8913ce3a25071de5e
truong21 <543@holbertonschool.com>
2019-05-18 01:28:30 +0000

    amenity.name is stored when checkbox is clicked

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,8 +2,15 @@ $(function () {
   $('input').on('click', function () {
     let list = []
     if (this.checked) {
-      //alert($('input').attr('data-name'));
-      $('h4').html('hey');
-    }
+        list.push(this.getAttribute('data-name'));
+        console.log(list)
+        $('amenities h4').text("");
+        $.each(list, function (index, element) {
+            if (index !=0) {
+                $('.amenities h4').append(", ");
+            }
+            $('.amenities h4').append(element);
+    });
+  }
   });
 });
