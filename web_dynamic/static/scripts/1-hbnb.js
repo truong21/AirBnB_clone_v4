4d32be02b5f0d1dcbb9f88e65faef631a8e1e53f
Wendy Leung <wendy.leung@holbertonschool.com>
2019-05-18 01:10:18 +0000

    add test script for checkbox amenity

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('input').on('click', function () {
+    let list = []
+    if (this.checked) {
+      //alert($('input').attr('data-name'));
+      $('h4').html('hey');
+    }
+  });
+});
